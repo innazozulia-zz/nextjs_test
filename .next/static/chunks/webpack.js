32edbb3ffecca1871f182d5f20db020309a6c2a8
inna <innazozulia@outlook.com>
2022-09-01 15:02:02 +0300

    fix naming

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d15e4c4845d73705"; }
+/******/ 		__webpack_require__.h = function() { return "ae12a5e60209a132"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
